bee99e40feb8799e0a10a3cfeeea0d268870397c
Lyagushka <rayz.sir@gmail.com>
2023-11-13 19:50:03 +0400

    fix

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
 import ethers from "ethers";
 import * as zksync from "zksync";
 export { zksync };
-export declare function getEthers5Signer(privateKey: string): ethers.Wallet;
+export declare function getEthers5Signer(privateKey: string): ethers.Signer;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import ethers from "ethers";
 import * as zksync from "zksync";
 export { zksync };
 
-export function getEthers5Signer(privateKey: string): ethers.Wallet {
+export function getEthers5Signer(privateKey: string): ethers.Signer {
   const provider = new ethers.providers.JsonRpcProvider("https://eth.llamarpc.com");
   return new ethers.Wallet(privateKey, provider);
 }
